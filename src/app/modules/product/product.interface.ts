78430103e05bba0d5bc4c78b8bf933ae7af7fd99
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-01-21 14:03:08 +0600

    Product interface create done

---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,20 @@
+
+export interface IProduct {
+  name: string
+  brand: string
+  price: number
+  category: 'writing' | 'office supplies' | 'art supplies'| 'Educational' | 'Technology'
+  description: string
+  quantity: number
+  inStock: number
+  
+}
+
+
+//  (string): The name of the product (e.g., Pen, Notebook, Eraser).
+//  (string): The brand of the product (e.g., Pilot, Moleskine, Faber-Castell).
+//  (number): Price of the product.
+//  (string): The type of product, using an enum (e.g., Writing, Office Supplies). use enum, exact value (Writing, Office Supplies, Art Supplies, Educational, Technology)
+// description (string): A brief description of the product.
+// quantity (number): Quantity of the product available.
+// inStock 
---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,92 @@
+
+import { model, Schema } from 'mongoose'
+import { IProduct } from './product.interface'
+
+//   name: string
+//   brand: string
+//   price: number
+//   category: 'writing' | 'office supplies' | 'art supplies'| 'Educational' | 'Technology'
+//   description: string
+//   quantity: number
+//   inStock: number
+  
+const productSchema = new Schema<IProduct>({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+     type: String,
+      enum: ['writing' , 'office supplies' ,'art supplies', 'Educational' , 'Technology'],
+    //   default: 'pending',
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+   quantity: {
+    type: Number,
+    required: true,
+    },
+    inStock: {
+    type: Number,
+    required: true,
+  },
+})
+
+// StaticRange, instance
+
+// tourSchema.methods.getNextNearestStartDateAndEndData = function () {
+//   const today = new Date()
+
+//   const futureDates = this.startDates.filter((startDate: Date) => {
+//     return startDate > today
+//   })
+
+//   futureDates.sort((a: Date, b: Date) => a.getTime() - b.getDate())
+
+//   const nearestStartDate = futureDates[0]
+//   const estimatedEndDate = new Date(
+//     nearestStartDate.getTime() + this.durationHours * 60 * 60 * 1000
+//   )
+
+//   return {
+//     nearestStartDate,
+//     estimatedEndDate,
+//   }
+// }
+
+// productSchema.static(
+//   'getNextNearestStartDateAndEndData',
+//   function getNextNearestStartDateAndEndData() {
+//     const today = new Date()
+
+//     const futureDates = this.startDates.filter((startDate: Date) => {
+//       return startDate > today
+//     })
+
+//     futureDates.sort((a: Date, b: Date) => a.getTime() - b.getDate())
+
+//     const nearestStartDate = futureDates[0]
+//     const estimatedEndDate = new Date(
+//       nearestStartDate.getTime() + this.durationHours * 60 * 60 * 1000
+//     )
+
+//     return {
+//       nearestStartDate,
+//       estimatedEndDate,
+//     }
+//   }
+// )
+
+const Product = model<IProduct>('Product', productSchema)
+
+export default Product
